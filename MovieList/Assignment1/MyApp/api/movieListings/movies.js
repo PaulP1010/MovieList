69e65c0014c31aff8aa72dbfd4ac27f3644de2ca
PaulP1010 <35928350+PaulP1010@users.noreply.github.com>
2018-03-17 23:11:12 +0000

    Add files via upload

---
+++ MovieList/Assignment1/MyApp/api/movieListings/movies.js
@@ -0,0 +1,30 @@
+export const movies = [
+  {
+    'name': 'Mad Max: Fury Road',
+	'year': '2015',
+	'genre': 'Action, Adventure, Sci-Fi, Thriller',
+    'runtime': '120 mins',
+    'rating': '4.5/5',
+  },
+  {
+    'name': 'The Dark Knight Rises',
+	'year': '2012',
+	'genre': 'Action, Thriller',
+    'runtime': '164 mins',
+    'rating': '3.9/5',
+  },
+  {
+    'name': 'Star Wars: The Last Jedi',
+	'year': '2017',
+	'genre': 'Action',
+    'runtime': '152 mins',
+    'rating': '3.7/5',
+  },
+  {
+    'name': 'Kong: Skull Island',
+	'year': '2017',
+	'genre': 'Action',
+    'runtime': '118 mins',
+    'rating': '3.4/5',
+  },
+];
